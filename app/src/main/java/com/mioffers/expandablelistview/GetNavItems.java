b1205a669d04f03c91b3a86bf99fff25fdaaa4e5
LaxmanMuttineni <iitdlaxman@gmail.com>
2016-02-06 15:11:35 +0530

    reminder

--- app/src/main/java/com/mioffers/expandablelistview/GetNavItems.java
+++ app/src/main/java/com/mioffers/expandablelistview/GetNavItems.java
@@ -13,9 +13,14 @@ public class GetNavItems {
 
     public ArrayList<NavItem> getNavItems(){
         ArrayList<NavItem> mNavItems= new ArrayList<>();
-        mNavItems.add(new NavItem("Home", "Meetup destination", R.drawable.ic_launcher));
-        mNavItems.add(new NavItem("Preferences", "Change your preferences", R.drawable.ic_launcher));
+        mNavItems.add(new NavItem("Profile", "change settings", R.drawable.ic_launcher));
+        mNavItems.add(new NavItem("Invite", "invite your friends", R.drawable.ic_launcher));
+        mNavItems.add(new NavItem("Reminders", "offers want to use", R.drawable.ic_launcher));
+        mNavItems.add(new NavItem("Shared", "by me/ for me / notifications", R.drawable.ic_launcher));
+        mNavItems.add(new NavItem("Promotions", "special offers for you", R.drawable.ic_launcher));
+        //mNavItems.add(new NavItem("Home", "Meetup destination", R.drawable.ic_launcher));
+        //mNavItems.add(new NavItem("Preferences", "Change your preferences", R.drawable.ic_launcher));
         mNavItems.add(new NavItem("About", "Get to know about us", R.drawable.ic_launcher));
-        return mNavItems;
+        mNavItems.add(new NavItem("Post", "post offers", R.drawable.ic_launcher)); return mNavItems;
     }
 }
